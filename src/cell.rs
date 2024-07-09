3023ff74d5d841107243adcd798f6ca4508a6e5a
Logic <38597904+LogicFan@users.noreply.github.com>
2024-07-09 03:08:09 -0400

    ad VoronoiCellNeighbor

--- src/cell.rs
+++ src/cell.rs
@@ -155,15 +155,18 @@ pub mod ffi {
             y: f64,
             z: f64,
         );
-        fn volume(self: Pin<&mut voronoicell_neighbor>) -> f64;
+        fn volume(
+            self: Pin<&mut voronoicell_neighbor>,
+        ) -> f64;
         fn max_radius_squared(
             self: Pin<&mut voronoicell_neighbor>,
         ) -> f64;
         fn total_edge_distance(
             self: Pin<&mut voronoicell_neighbor>,
         ) -> f64;
-        fn surface_area(self: Pin<&mut voronoicell_neighbor>)
-            -> f64;
+        fn surface_area(
+            self: Pin<&mut voronoicell_neighbor>,
+        ) -> f64;
         fn centroid(
             self: Pin<&mut voronoicell_neighbor>,
             cx: &mut f64,
@@ -404,6 +407,63 @@ impl VoronoiCell for VoronoiCellNoNeighbor {
         let mut v = CxxVector::new();
         self.inner.pin_mut().vertex_orders(v.pin_mut());
         v.into_iter().copied().collect()
+    }
+}
+
+pub struct VoronoiCellNeighbor {
+    inner: UniquePtr<ffi::voronoicell_neighbor>,
+}
+
+impl VoronoiCellNeighbor {
+    /// Initializes the Voronoi cell to be rectangular box with the
+    /// given dimensions.
+    ///
+    /// * `xyz_min`: the minimum xyz coordinates.
+    /// * `xyz_max`: the maximum xyz coordinates.
+    pub fn new(xyz_min: Vec3, xyz_max: Vec3) -> Self {
+        let mut val = Self {
+            inner: ffi::new_voronoicell_neighbor(),
+        };
+        val.inner.pin_mut().init_base(
+            xyz_min[0], xyz_max[0], xyz_min[1], xyz_max[1],
+            xyz_min[2], xyz_max[2],
+        );
+        val
+    }
+
+    /// Initializes the cell to be an octahedron with vertices at
+    /// (l,0,0), (-l,0,0), (0,l,0), (0,-l,0), (0,0,l), and (0,0,-l).
+    ///
+    /// * `l`: a parameter setting the size of the octahedron.
+    pub fn new_octahedron(l: f64) -> Self {
+        let mut val = Self {
+            inner: ffi::new_voronoicell_neighbor(),
+        };
+        val.inner.pin_mut().init_octahedron_base(l);
+        val
+    }
+
+    /// Initializes the cell to be a tetrahedron.
+    ///
+    /// * `xyz0`: the coordinates of the first vertex.
+    /// * `xyz1`: the coordinates of the second vertex.
+    /// * `xyz2`: the coordinates of the third vertex.
+    /// * `xyz3`: the coordinates of the fourth vertex.
+    pub fn new_tetrahedron(
+        xyz0: Vec3,
+        xyz1: Vec3,
+        xyz2: Vec3,
+        xyz3: Vec3,
+    ) -> Self {
+        let mut val = Self {
+            inner: ffi::new_voronoicell_neighbor(),
+        };
+        val.inner.pin_mut().init_tetrahedron_base(
+            xyz0[0], xyz0[1], xyz0[2], xyz1[0], xyz1[1],
+            xyz1[2], xyz2[0], xyz2[1], xyz2[2], xyz3[0],
+            xyz3[1], xyz3[2],
+        );
+        val
     }
 }
 
