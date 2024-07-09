65778dcf86a9ef9057ae1da7723caa2cee00f74c
Logic <38597904+LogicFan@users.noreply.github.com>
2024-07-09 11:40:26 -0400

    fix macos build

--- build.rs
+++ build.rs
@@ -29,6 +29,7 @@ fn main() {
     cxx_build::bridges(["src/cell.rs"])
         .file("cpp/voro++.cc")
         .cpp(true)
+        .std("c++14")
         .warnings(false)
         .compile("voro++");
 }
