6225d1c943d1ed8dc5d6ddb1dd075b7c52911753
Logic <38597904+LogicFan@users.noreply.github.com>
2024-07-10 18:31:59 -0400

    impl Walls for ContainerStd

--- src/container.rs
+++ src/container.rs
@@ -3,6 +3,13 @@ pub mod ffi {
     unsafe extern "C++" {
         include!("voro_rs/cpp/boilerplate.hh");
 
+        type voronoicell = crate::cell::ffi::voronoicell;
+        type voronoicell_neighbor =
+            crate::cell::ffi::voronoicell_neighbor;
+
+        type wall = crate::wall::ffi::wall;
+        type wall_list = crate::wall_list::ffi::wall_list;
+
         type container;
         #[rust_name = "new_container"]
         fn construct(
@@ -20,21 +27,59 @@ pub mod ffi {
             zperiodic_: bool,
             init_mem: i32,
         ) -> UniquePtr<container>;
-        // TODO: method from container_base
-        fn clear(
+
+        unsafe fn add_wall(
             self: Pin<&mut container>,
+            w: Pin<&mut wall>,
         );
+        #[rust_name = "add_walls"]
+        unsafe fn add_wall(
+            self: Pin<&mut container>,
+            w: Pin<&mut wall_list>,
+        );
+        fn point_inside_walls(
+            self: Pin<&mut container>,
+            x: f64,
+            y: f64,
+            z: f64,
+        ) -> bool;
+        #[rust_name = "apply_walls_0"]
+        fn apply_walls(
+            self: Pin<&mut container>,
+            c: Pin<&mut voronoicell>,
+            x: f64,
+            y: f64,
+            z: f64,
+        ) -> bool;
+        #[rust_name = "apply_walls_1"]
+        fn apply_walls(
+            self: Pin<&mut container>,
+            c: Pin<&mut voronoicell_neighbor>,
+            x: f64,
+            y: f64,
+            z: f64,
+        ) -> bool;
+
+        fn point_inside(
+            self: Pin<&mut container>,
+            x: f64,
+            y: f64,
+            z: f64,
+        ) -> bool;
+        fn total_particles(
+            self: Pin<&mut container>,
+        ) -> i32;
+
+        fn clear(self: Pin<&mut container>);
         fn put(
             self: Pin<&mut container>,
             n: i32,
             x: f64,
             y: f64,
-            z: f64
+            z: f64,
         );
         // TODO: put with particle_order
-        fn compute_all_cells(
-            self: Pin<&mut container>,
-        );
+        fn compute_all_cells(self: Pin<&mut container>);
         fn sum_cell_volumes(
             self: Pin<&mut container>,
         ) -> f64;
@@ -46,8 +91,164 @@ pub mod ffi {
             rx: &mut f64,
             ry: &mut f64,
             rz: &mut f64,
-            pid: &mut i32
+            pid: &mut i32,
         ) -> bool;
         // TODO: compute_cell with c_loop
+        #[rust_name = "compute_cell_with_index_0"]
+        fn compute_cell(
+            self: Pin<&mut container>,
+            c: Pin<&mut voronoicell>,
+            ijk: i32,
+            q: i32,
+        ) -> bool;
+        #[rust_name = "compute_cell_with_index_1"]
+        fn compute_cell(
+            self: Pin<&mut container>,
+            c: Pin<&mut voronoicell_neighbor>,
+            ijk: i32,
+            q: i32,
+        ) -> bool;
+        #[rust_name = "compute_ghost_0"]
+        fn compute_ghost_cell(
+            self: Pin<&mut container>,
+            c: Pin<&mut voronoicell>,
+            x: f64,
+            y: f64,
+            z: f64,
+        ) -> bool;
+        #[rust_name = "compute_ghost_1"]
+        fn compute_ghost_cell(
+            self: Pin<&mut container>,
+            c: Pin<&mut voronoicell_neighbor>,
+            x: f64,
+            y: f64,
+            z: f64,
+        ) -> bool;
+    }
+}
+
+use crate::cell::VoroCellMut;
+use crate::wall::ffi::{
+    wall_cone_to_wall, wall_cylinder_to_wall,
+    wall_plane_to_wall, wall_sphere_to_wall,
+};
+use crate::wall::WallMut;
+use crate::wall_list::{Walls, WallsMut};
+use cxx::UniquePtr;
+use std::marker::PhantomData;
+
+type DVec3 = [f64; 3];
+type IVec3 = [i32; 3];
+type BVec3 = [bool; 3];
+
+// pub trait Container {
+//     fn
+// }
+
+pub struct ContainerStd<'a> {
+    pub(crate) inner: UniquePtr<ffi::container>,
+    phantom: PhantomData<&'a ()>,
+}
+
+impl<'a> ContainerStd<'a> {
+    pub fn new(
+        xyz_min: DVec3,
+        xyz_max: DVec3,
+        sub_grids: IVec3,
+        is_periodic: BVec3,
+        init_mem_alloc: i32,
+    ) -> Self {
+        Self {
+            inner: ffi::new_container(
+                xyz_min[0],
+                xyz_max[0],
+                xyz_min[1],
+                xyz_max[1],
+                xyz_min[2],
+                xyz_max[2],
+                sub_grids[0],
+                sub_grids[1],
+                sub_grids[2],
+                is_periodic[0],
+                is_periodic[1],
+                is_periodic[2],
+                init_mem_alloc,
+            ),
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a> Walls<'a> for ContainerStd<'a> {
+    fn add_wall(
+        &mut self,
+        wall: impl Into<crate::wall::WallMut<'a>>,
+    ) {
+        let w0 = match wall.into() {
+            WallMut::Sphere(w) => {
+                wall_sphere_to_wall(w.inner.pin_mut())
+            }
+            WallMut::Plane(w) => {
+                wall_plane_to_wall(w.inner.pin_mut())
+            }
+            WallMut::Cylinder(w) => {
+                wall_cylinder_to_wall(w.inner.pin_mut())
+            }
+            WallMut::Cone(w) => {
+                wall_cone_to_wall(w.inner.pin_mut())
+            }
+        };
+        unsafe {
+            // ensure the lifetime of `self` is within the lifetime of
+            // `wall` using the lifetime specifier `'a`.
+            self.inner.pin_mut().add_wall(w0);
+        }
+    }
+
+    fn add_walls(
+        &mut self,
+        walls: impl Into<crate::wall_list::WallsMut<'a>>,
+    ) {
+        unsafe {
+            // ensure the lifetime of `self` is within the lifetime of
+            // `wall` using the lifetime specifier `'a`.
+            match walls.into() {
+                WallsMut::WallList(w) => self
+                    .inner
+                    .pin_mut()
+                    .add_walls(w.inner.pin_mut()),
+            }
+        }
+    }
+
+    fn inside_walls(&mut self, xyz: DVec3) -> bool {
+        self.inner
+            .pin_mut()
+            .point_inside_walls(xyz[0], xyz[1], xyz[2])
+    }
+
+    fn apply_walls<'b>(
+        &mut self,
+        cell: impl Into<crate::cell::VoroCellMut<'b>>,
+        xyz: DVec3,
+    ) -> bool {
+        match cell.into() {
+            VoroCellMut::Sgl(c) => {
+                self.inner.pin_mut().apply_walls_0(
+                    c.inner.pin_mut(),
+                    xyz[0],
+                    xyz[1],
+                    xyz[2],
+                )
+            }
+            VoroCellMut::Nbr(c) => {
+                self.inner.pin_mut().apply_walls_1(
+                    c.inner.pin_mut(),
+                    xyz[0],
+                    xyz[1],
+                    xyz[2],
+                )
+            }
+        }
     }
 }
