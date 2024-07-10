be48c531369a719391d818c3c104342c2bf2541f
Logic <38597904+LogicFan@users.noreply.github.com>
2024-07-10 16:37:26 -0400

    add some container ffi

--- src/container.rs
+++ src/container.rs
@@ -4,6 +4,7 @@ pub mod ffi {
         include!("voro_rs/cpp/boilerplate.hh");
 
         type container;
+        #[rust_name = "new_container"]
         fn construct(
             ax_: f64,
             bx_: f64,
@@ -19,5 +20,34 @@ pub mod ffi {
             zperiodic_: bool,
             init_mem: i32,
         ) -> UniquePtr<container>;
+        // TODO: method from container_base
+        fn clear(
+            self: Pin<&mut container>,
+        );
+        fn put(
+            self: Pin<&mut container>,
+            n: i32,
+            x: f64,
+            y: f64,
+            z: f64
+        );
+        // TODO: put with particle_order
+        fn compute_all_cells(
+            self: Pin<&mut container>,
+        );
+        fn sum_cell_volumes(
+            self: Pin<&mut container>,
+        ) -> f64;
+        fn find_voronoi_cell(
+            self: Pin<&mut container>,
+            x: f64,
+            y: f64,
+            z: f64,
+            rx: &mut f64,
+            ry: &mut f64,
+            rz: &mut f64,
+            pid: &mut i32
+        ) -> bool;
+        // TODO: compute_cell with c_loop
     }
 }
