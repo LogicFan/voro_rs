e9df1f6249bceeaa25add682c95b4ebf9457cc3d
Logic <38597904+LogicFan@users.noreply.github.com>
2024-07-11 22:23:19 -0400

    complete particle marker docs

--- src/particle_marker.rs
+++ src/particle_marker.rs
@@ -15,15 +15,32 @@ pub mod ffi {
 
 use cxx::UniquePtr;
 
+/// A class for storing ordering information when particles are added to
+/// a container.
+/// 
+/// When particles are added to a container class, they are sorted into an
+/// internal computational grid of blocks. The `ParticleMarker` class provides a
+/// mechanism for remembering which block particles were sorted into. The import
+/// and put routines in the container class have variants that also take a
+/// `ParticleMarker` class. Each time they are called, they will store the block
+/// that the particle was sorted into, plus the position of the particle within
+/// the block. The `ParticleMarker` class can used by the `LoopMarked` class to
+/// specifically loop over the particles that have their information stored
+/// within it. 
 pub struct ParticleMarker {
     pub(crate) inner: UniquePtr<ffi::particle_order>,
 }
 
 impl ParticleMarker {
+    /// Create a new `ParticleMarker`.
     pub fn new() -> Self {
         Self::new_with_memory(16)
     }
 
+    /// The struct allocates memory to store the
+    /// ordering information.
+    /// * `initial_memory`: the initial amount of memory to allocate, 
+    /// in terms of particle count.
     pub fn new_with_memory(initial_memory: i32) -> Self {
         Self {
             inner: ffi::new_particle_order(initial_memory),
